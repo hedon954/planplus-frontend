acea9259f67460eaab0153a331feee2aae4b5f85
Arthur-0 <1015982558@qq.com>
2020-11-01 21:40:49 +0800

    userInfo

---
+++ pages/user-info/user-info.js
@@ -0,0 +1,99 @@
+Page({
+    data: {
+        userId:1,
+        userNickname:"kk",
+        userGender:-1,
+        userBirthday:"",
+        userAvatarUrl:"",
+        userGenderText:""
+    },
+
+    goToDetail:function(id){
+        swan.navigateTo({
+            url: '../../pages/user-info-detail/user-info-detail'
+        });
+    },
+
+    getUserInfo:function(e){
+        swan.request({
+            url: 'http://127.0.0.1:10030/project/user/2',
+            method:"GET",
+            success:res=>{
+                console.log("①");
+                console.log(res.data.data);
+                this.setData(
+                    {
+                        userNickname:res.data.data.userNickname,
+                        userAvatarUrl:res.data.data.userAvatarUrl,
+                        userGender:res.data.data.userGender,
+                    }
+                )
+            }
+        });
+    },
+
+
+    onLoad: function () {
+        // 监听页面加载的生命周期函数
+        swan.request({
+            url: 'http://127.0.0.1:10030/project/user/2',
+            method:"GET",
+            success:res=>{
+                console.log("①");
+                console.log(res.data.data);
+                this.setData(
+                    {
+                        userNickname:res.data.data.userNickname,
+                        userAvatarUrl:res.data.data.userAvatarUrl,
+                        userGender:res.data.data.userGender,
+                    }
+                )
+                switch(this.data.userGender)
+                {
+                    case 0:
+                        this.setData(
+                            {
+                               userGenderText:"女",
+                            }
+                        );
+                        break;
+                    case 1:
+                        this.setData(
+                            {
+                                userGenderText:"男",
+                            }
+                        );
+                        break;
+                    default:
+                        this.setData(
+                            {
+                                userGenderText:"未知",
+                            }
+                        );
+                        break;
+                }
+            }
+        });
+    },
+    onReady: function() {
+        // 监听页面初次渲染完成的生命周期函数
+    },
+    onShow: function() {
+        // 监听页面显示的生命周期函数
+    },
+    onHide: function() {
+        // 监听页面隐藏的生命周期函数
+    },
+    onUnload: function() {
+        // 监听页面卸载的生命周期函数
+    },
+    onPullDownRefresh: function() {
+        // 监听用户下拉动作
+    },
+    onReachBottom: function() {
+        // 页面上拉触底事件的处理函数
+    },
+    onShareAppMessage: function () {
+        // 用户点击右上角转发
+    }
+});
