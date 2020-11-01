d1164a51e863c7c21a1aa210c7f5da06b18313fa
yangj0020 <1453370940@qq.com>
2020-11-01 20:13:46 +0800

    提交到本地库

---
+++ pages/home/home.js
@@ -0,0 +1,93 @@
+Page({
+    data: {
+        activeName: ''
+    },
+
+    login: function() {
+        swan.request({
+            url: 'http://localhost:10030/project/task/today',
+            method: 'GET',
+            dataType: 'json',
+            responseType: 'application/json',
+            data: {
+                userId: 1
+            },
+            success: res => {
+                try {
+                    swan.showModal({
+                        title: '请求到的数据',
+                        content: JSON.stringify(res.data),
+                        showCancel: false
+                    });
+                }
+                catch (error) {
+                    console.log(error);
+                }
+            }
+        });
+    },
+
+    getTasks: function(e) {
+        swan.request({
+            url: 'http://localhost:10030/project/task/' + e.detail.name,
+            method: 'GET',
+            dataType: 'json',
+            responseType: 'application/json',
+            data: {
+                userId: 1
+            },
+            success: res => {
+                try {
+                    swan.showModal({
+                        title: '请求到的数据',
+                        content: JSON.stringify(res.data),
+                        showCancel: false
+                    });
+                    this.setData({activeName: e.detail.name
+                    });
+                }
+                catch (error) {
+                    console.log(error);
+                }
+            }
+        });
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    onLoad: function () {
+        // 监听页面加载的生命周期函数
+    },
+    onReady: function() {
+        // 监听页面初次渲染完成的生命周期函数
+    },
+    onShow: function() {
+        // 监听页面显示的生命周期函数
+    },
+    onHide: function() {
+        // 监听页面隐藏的生命周期函数
+    },
+    onUnload: function() {
+        // 监听页面卸载的生命周期函数
+    },
+    onPullDownRefresh: function() {
+        // 监听用户下拉动作
+    },
+    onReachBottom: function() {
+        // 页面上拉触底事件的处理函数
+    },
+    onShareAppMessage: function () {
+        // 用户点击右上角转发
+    }
+});
