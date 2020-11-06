ce8776d4f6b356790fef56613d36080d1941fc8a
yangj0020 <1453370940@qq.com>
2020-11-06 20:13:59 +0800

    日期时间选择器逻辑修改

--- pages/modification/modification.js
+++ pages/modification/modification.js
@@ -5,12 +5,12 @@ Page({
     data: {
         taskId: 0,
         content: "", //任务内容
-        startDate: '', //开始日期
-        startTime: '', //开始时间
-        endDate: '', //结束日期
-        endTime: '', //结束时间
+        startDate: '', //任务开始日期
+        startTime: '', //任务开始时间
+        endDate: '', //任务结束日期
+        endTime: '', //任务结束时间
         place: '', //任务地点
-        frequencyList: [{
+        frequencyList: [{ //任务频率列表，作为选择器显示内容的范围
             id: '0',
             name:'仅一次'
         }, {
@@ -23,9 +23,9 @@ Page({
             id: '3',
             name: '每月'
         }],
-        frequencyIndex: 0,
-        frequency: 0,
-        aheadTimeList: [{
+        frequencyIndex: 0, //表示选择器中选中值在列表中的下标
+        frequency: 0, //任务频率
+        aheadTimeList: [{ //提前提醒时间列表，作为选择器显示内容的范围
             id: '0',
             name:'5分钟'
         }, {
@@ -47,13 +47,17 @@ Page({
             id: '6',
             name:'5小时'
         }],
-        aheadTimeIndex: 0,
-        aheadIndexMap: [5, 10, 20, 30, 60, 120, 300],
+        aheadTimeIndex: 0, //表示选择器中选中值在列表中的下标
+        aheadIndexMap: [5, 10, 20, 30, 60, 120, 300], //下标与频率值（以分钟为单位）的映射
         aheadTime: 5, //提前提醒时间
-        // startDateStart: '', //开始时间的日期起点
-        // startTimeStart: '', //开始时间的时间起点
-        // endDatestart: '', //结束时间的日期起点
-        // endTimeStart: '', //结束时间的时间起点
+        startDateStart: '', //开始时间的日期起点
+        startDateDisplay: '', //打开日期选择器时默认选中（显示）的日期/开始日期
+        startTimeStart: '', //开始时间的时间起点
+        startTimeDisplay: '', //打开时间选择器时默认选中（显示）的时间/开始时间
+        endDateStart: '', //结束时间的日期起点
+        endDateDisplay: '', //打开日期选择器时默认选中（显示）的日期/结束日期
+        endTimeStart: '', //结束时间的时间起点
+        endTimeDisplay: '', //打开时间选择器时默认选中（显示）的时间/结束时间
     },
     onLoad(options) {
         console.log(options);
@@ -62,8 +66,6 @@ Page({
         // console.log(myDate.getHours() + ':' + myDate.getMinutes());
         this.setData({
             taskId: options.taskId
-            // startDateStart: myDate.getFullYear() + '-' + myDate.getMonth() + '-' + myDate.getDate(),
-            // startTimeStart: myDate.getHours() + ':' + myDate.getMinutes()
         });
         swan.request({
             url: 'http://localhost:9527/project/task/single/' + this.data.taskId,
@@ -84,6 +86,7 @@ Page({
                     console.log('开始赋值');
 
                     this.setData({
+                        //给任务信息赋值
                         content: response['taskContent'],
                         place: response['taskPlace'],
                         startDate: response['taskStartTime'].substring(0, 10),
@@ -92,7 +95,30 @@ Page({
                         endTime: response['taskPredictedFinishTime'].substring(11, 16),
                         frequency: response['taskRate'],
                         frequencyIndex: response['taskRate'],
-                        aheadTime: response['taskAdvanceRemindTIme'],
+                        aheadTime: response['taskAdvanceRemindTIme']
+                    });
+                    //初始化日期和时间选择器相关变量
+                    var nowPlusFiveMinutes = myDate.getHours() + ":" + myDate.getMinutes();
+                    var tmp1 = timeCal(nowPlusFiveMinutes, 5);
+                    nowPlusFiveMinutes = tmp1[0];
+
+                    var tmp2 = timeCal(nowPlusFiveMinutes, 1);
+                    var nowPlusSixMinutes = tmp2[0];
+
+                    var today = myDate.toLocaleDateString().replace(/\//g, '-');
+                    console.log('今天是：' + today);
+                    var tomorrow = this.dateCal(today, 1+tmp[1]);
+                    console.log('明天是：' + tomorrow);
+                    var nextday = this.dateCal(this.data.startDate, 1); //任务开始的后一天
+                    this.setData({
+                        startDateStart: today,
+                        startDateDisplay: tomorrow,
+                        startTimeStart: nowPlusFiveMinutes,
+                        startTimeDisplay: this.timeCal(nowPlusFiveMinutes, 1)[0],
+                        endDateStart: this.data.startDate,
+                        endDateDisplay: nextday,
+                        endTimeStart: this.data.startTime,
+                        endTimeDisplay: this.timeCal(this.data.startTime, 1)[0]
                     });
                 }
                 catch (error) {
@@ -100,6 +126,32 @@ Page({
                 }
             },
         });
+    },
+
+    //日期加减
+    dateCal(paramDate, n) {
+        let dateStr = paramDate.replace(/\//g, '-');
+        let date = new Date(dateStr);
+        date.setDate(date.getDate() + n);
+        return date.toLocaleDateString().replace(/\//g, '-');
+    },
+    //时间（时：分）加减
+    timeCal(paramTime, n) {
+        let mm = parseInt(paramTime.substring(3));
+        let hh = parseInt(paramTime.substring(0, 2));
+        let carryIn = 0; //表示日期是否需要进位
+        if(mm + n < 60) {
+            mm += n;
+        } else {
+            mm = (mm + n) % 60;
+            if(hh < 23) {
+                hh += 1;
+            } else {
+                hh = '00';
+                carryIn = 1;
+            }
+        }
+        return [`${hh}:${mm}`, carryIn];
     },
 
     //日期修改
