cca8c5994c7bf9e4b53fbe71d8b19529b47c43ba
Hedon954 <171725713@qq.com>
2020-11-30 18:44:56 +0800

    删掉 app.js 里面的 token

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ App({
     data:{
         subScribeId: 0,
         taskChanged: false,
-        access_token:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2MDQ5ODgxNjEsInVzZXJfbmFtZSI6IjE1NjIzMjA1MTU2IiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9BRE1JTiJdLCJqdGkiOiI5NGVjODUwNy0xODMxLTQ5MWMtYjY2NS03ZTdkMjZjY2UzZDkiLCJjbGllbnRfaWQiOiJwbGFucGx1cyIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdfQ.ItYzJaLyTSSXnE0MWx2VqNJXTw6v7mXe0RJmf1RG-Dc',
+        access_token:'',
         infoChanged:false,
     },
 
